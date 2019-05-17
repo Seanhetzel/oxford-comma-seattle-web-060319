11a0669125910994e0849bb0e343cf09c8c6cbec
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-17 01:54:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length == 3
-    array[0, 2].join(", ") << ", and #{array[2]}" 
-  elsif array.length > 3
+  #elsif array.length == 3
+  #  array[0, 2].join(", ") << ", and #{array[2]}" 
+  elsif array.length > 2
     array[0, array.length - 1].join(", ") << ", and #{array[array.length - 1]}"
   end
 end
