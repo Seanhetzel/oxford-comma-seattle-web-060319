2a3c59e378310e016143a2b7d3c17df7b2dd543f
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-17 01:54:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,6 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  #elsif array.length == 3
-  #  array[0, 2].join(", ") << ", and #{array[2]}" 
   elsif array.length > 2
     array[0, array.length - 1].join(", ") << ", and #{array[array.length - 1]}"
   end
